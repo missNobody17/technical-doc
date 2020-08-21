d25131cf08cc4b196c7e4e4b017f95b7ac975d58
missNobody17 <59320029+missNobody17@users.noreply.github.com>
2020-08-21 20:30:58 +0100

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# technical document about unicorns
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/cocopixie/pen/YzqPvWj](https://codepen.io/cocopixie/pen/YzqPvWj).
+
+
---
+++ script.js
@@ -0,0 +1,29 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+
+
+
+$(document).ready(function(){
+  $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+    
+    $(".zoom").hover(function(){
+		
+		$(this).addClass('transition');
+	}, function(){
+        
+		$(this).removeClass('transition');
+	});
+});
+    
